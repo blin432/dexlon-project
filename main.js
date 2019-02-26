6dda623a76f36c8a512eb4fd2bfefe7e0a7c3733
benjamin lin <blin432@gmail.com>
2019-02-25 23:20:29 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const anime = require('lib/anime.js');
+anime({
+    targets: '.about-me-section',
+    translateX: 250,
+    rotate: '1turn',
+    backgroundColor: '#FFF',
+    duration: 800
+  });
